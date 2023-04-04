a4f722106e601a4098e2d200a7b13252b53fab50
Michael Mera <michael.mera@cispa.de>
2023-04-04 21:53:31 +0200

    feat: implement rock paper scissors tournament

---
+++ rps.js
@@ -0,0 +1,70 @@
+const choices = ['rock', 'paper', 'scissors'];
+let PlayerScore = 0;
+let ComputerScore = 0;
+
+function game() {
+    play(getPlayerChoice());
+    play(getPlayerChoice());
+    play(getPlayerChoice());
+    play(getPlayerChoice());
+    play(getPlayerChoice());
+
+    let message;
+    if (PlayerScore == ComputerScore) {
+        message = `You're as strong as the computer!`;
+    } else if (PlayerScore > ComputerScore) {
+        message = `You have beaten the computer. Congrats!`;
+    } else {
+        message = `You lose. Try again`;
+    }
+
+    console.log(`[${PlayerScore} - ${ComputerScore}] ${message}`);
+}
+
+
+function play(playerSelection, computerSelection = getComputerChoice()) {
+    if (!isValidChoice(playerSelection)) {
+        console.log(`Your choice is not a valid move! You forfeit this round.`);
+        ComputerScore += 1;
+        return;
+    }
+
+    if (playerSelection == computerSelection) {
+        console.log(`It's a draw! You both chose ${playerSelection}`);
+        return;
+    }
+
+    if (beats(playerSelection, computerSelection)) {
+        PlayerScore += 1;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    } else {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        ComputerScore += 1;
+    }
+}
+
+function beats(selection1, selection2) {
+    const result = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper',
+    };
+
+    return (result[selection1] == selection2);
+}
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function getPlayerChoice() {
+    let choice = (prompt('Enter your choice (rock/paper/scissors):') ?? '').toLowerCase();
+
+    return choice;
+}
+
+function isValidChoice(choice) {
+    return (choices.includes(choice));
+}
+
+game();
